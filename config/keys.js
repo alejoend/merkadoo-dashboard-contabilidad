0577c44a87b8599d50f0472db29464c9edf3c927
Norberto Caceres <norbe1994@yahoo.com>
2019-01-19 17:49:08 -0500

    restart project

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://caceres-norberto:MerkadooContabilidad3914969!@ds161144.mlab.com:61144/merkadoo-dashboard-contabilidad",
-  secretOrKey: "superSecreto3000"
-};
