4ebe9fa8e58d7c4566032c24343b2e34c697a732
Norberto Caceres <norbe1994@yahoo.com>
2019-01-31 15:50:48 -0500

    Agregar estructura basica de front-end con Vue

---
+++ vue-cli/src/components/Shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="">
+</style>
---
+++ vue-cli/src/components/Shared/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <header>
+        <h1>Heading</h1>
+      </header>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
